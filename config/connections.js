f395dfb4f856f051af4e0edf22771fb47d92cd07
Lucas Krahl <luckrahl1996@gmail.com>
2017-06-19 15:21:32 -0300

    bkp

--- config/connections.js
+++ config/connections.js
@@ -76,11 +76,11 @@ module.exports.connections = {
   ***************************************************************************/
    somePostgresqlServer: {
      adapter: 'sails-postgresql',
-     host: 'ec2-184-72-249-88.compute-1.amazonaws.com',
-     database: 'd4508i6c2ufpgf',
-     user: 'wydcfxpiolftbg',
-     password: '4417ec71940c7cccc9bd013647cccbda767a8bf43eacfa36aad0f28545fafa0e',
-     ssl      : true //optional
+     host: 'localhost',
+     database: 'gp-calendar',
+     user: 'postgres',
+     password: 'postgres'//,
+     //ssl      : true //optional
    }
 
 
